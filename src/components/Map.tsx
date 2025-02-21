dae5d9f95f67792bef5552ece7be8f5a286f018e
Rob Shearer <shearer.robert@gmail.com>
2025-02-21 11:56:04 -0600

    Move AOI type declaration to local scope in Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,9 +8,19 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import 'leaflet-draw';
-import { AOI } from '@/types/aoi';
 import * as geojson from 'geojson';
 import { randomUUID } from 'crypto';
+
+// Local type declaration for AOI
+type AOI = {
+  id: string;
+  mgrsCoordinate: string;
+  dimensions: string;
+  bounds: [number, number][];
+  name: string;
+  dateCreated: string;
+  layer?: L.Layer;
+};
 
 const { BaseLayer } = LayersControl;
 
@@ -58,16 +68,6 @@ const storage = {
     return [];
   }
 };
-
-interface AOI {
-  id: string;
-  mgrsCoordinate: string;
-  dimensions: string;
-  bounds: [number, number][];
-  name: string;
-  dateCreated: string;
-  layer?: L.Layer;
-}
 
 // Utility function to convert AOI to GeoJSON Feature
 const aoiToGeoJSON = (aoi: AOI): geojson.Feature => {
@@ -816,7 +816,7 @@ export default function Map() {
                 className="text-red-500 hover:text-red-700 p-1"
                 title="Delete All"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0111 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
                 </svg>
               </button>
